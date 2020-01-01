9cc24764854b19cc59dbc585b9211b942a15a043
luk3my <luk3myoung@gmail.com>
2020-01-01 20:05:22 +1000

    apollo query

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,13 +1,63 @@
 <template>
   <div class="container">
-    <h1>Home</h1>
-    <ul v-for="post in getPosts" :key="post._id">
-      <li>
-        {{post.title}}
-        {{post.imageUrl}} 
-        {{post.description}} 
-      </li>
-    </ul>
+
+    <!-- <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="./lake.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="./asphalt-autumn.jpg" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="./lake.jpg" alt="Third slide">
+        </div>
+      </div>
+    </div> -->
+
+   <!-- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="https://placeimg.com/1080/500/animals" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>My Caption Title (1st Image)</h5>
+                <p>The whole caption will only show up if the screen is at least medium size.</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="https://placeimg.com/1080/500/arch" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="https://placeimg.com/1080/500/nature" alt="Third slide">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div> -->
+
+    <!-- <ApolloQuery :query="getPostsQuery">
+      <template slot-scope="{ result: { loading, error, data } }">
+        <div v-if="loading">Loading...</div>
+        <div v-else-if="error">ERROR! {{error.message}}</div>
+        <ul v-else v-for="post in data.getPosts" :key="post._id">
+          <li>
+            {{post.title}} {{post.imageUrl}} {{post.likes}}
+          </li>
+        </ul>
+      </template>
+    </ApolloQuery> -->
+
   </div>
 </template>
 
@@ -16,9 +66,9 @@
 
   export default {
     name: 'home',
-    apollo: {
-      getPosts: {
-        query: gql`
+    data() {
+      return {
+        getPostsQuery: gql`
         query {
           getPosts {
           _id
@@ -28,7 +78,7 @@
           likes
           }
         }
-      `
+        `
       }
     }
   }
